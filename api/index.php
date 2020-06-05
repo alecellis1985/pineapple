ca25d15487e8414e587e123c6e2b5c03fb570978
alecellis1985 <alecellis1985@gmail.com>
2020-06-05 12:32:49 -0300

    style updates

--- api/index.php
+++ api/index.php
@@ -1,17 +1,11 @@
 <?php
 
-require 'Slim/Slim.php';
-
-require_once("Slim/includes/class.Conexion.BD.php");
-require_once("Slim/config/parametros.php");
-require_once("Slim/includes/MessageHandler.php");
-
-require_once('mailHandler.php');session_cache_limiter(false);
-session_start();
-$app = new Slim();
-
-
-
-$app->post('/sendMail', 'sendEmail');
-
+require 'Slim/Slim.php';
+require_once("Slim/includes/class.Conexion.BD.php");
+require_once("Slim/config/parametros.php");
+require_once("Slim/includes/MessageHandler.php");
+require_once('mailHandler.php');session_cache_limiter(false);
+session_start();
+$app = new Slim();
+$app->post('/sendMail', 'sendEmail');
 $app->run();
